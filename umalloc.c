8da672f4b6cc3a9eed0e3e68a779054eae1aba7e
Ryanho12123 <113861129+Ryanho12123@users.noreply.github.com>
2023-09-19 17:39:23 -0500

    Update umalloc.c

--- umalloc.c
+++ umalloc.c
@@ -4,7 +4,7 @@
 #include <assert.h>
 #include "ansicolors.h"
 
-const char author[] = ANSI_BOLD ANSI_COLOR_RED "REPLACE THIS WITH YOUR NAME AND UT EID" ANSI_RESET;
+const char author[] = ANSI_BOLD ANSI_COLOR_RED "Ryan Ho rdh2837" ANSI_RESET;
 
 /*
  * The following helpers can be used to interact with the memory_block_t
@@ -94,6 +94,16 @@ memory_block_t *get_block(void *payload) {
  */
 memory_block_t *find(size_t size) {
     //* STUDENT TODO
+    memory_block_t *current_block = free_head;
+    while (current_block != NULL) {
+        // Check if the current block is large enough
+        if (get_size(current_block) >= size) {
+            return current_block; // Found a suitable block
+        }
+        current_block = get_next(current_block); // Move to the next block in the free list
+    }
+    
+    // No suitable block found
     return NULL;
 }
 
@@ -102,7 +112,27 @@ memory_block_t *find(size_t size) {
  */
 memory_block_t *extend(size_t size) {
     //* STUDENT TODO
-    return NULL;
+    size_t total_size = size + sizeof(memory_block_t);
+    
+    // Request additional memory from the operating system
+    void *new_heap = csbrk(total_size);
+    
+    // Check if the csbrk call failed (returned NULL)
+    if (new_heap == NULL) {
+        return NULL; // Failed to extend the heap
+    }
+    
+    // Create a new memory block structure for the extended heap
+    memory_block_t *new_block = (memory_block_t *)new_heap;
+    
+    // Mark the new block as free and set its size
+    put_block(new_block, total_size, 0);
+    
+    // Add the new block to the free list
+    new_block->next = free_head;
+    free_head = new_block;
+    
+    return new_block; // Return the extended block
 }
 
 /*
@@ -110,7 +140,65 @@ memory_block_t *extend(size_t size) {
  */
 memory_block_t *split(memory_block_t *block, size_t size) {
     //* STUDENT TODO
-    return NULL;
+    // assert(block != NULL);
+
+    // size_t block_size = get_size(block);
+
+    // // Check if splitting is possible
+    // if (block_size < size + sizeof(memory_block_t)) {
+    //     return NULL;  // Cannot split, not enough space for a new block.
+    // }
+
+    // // Calculate the size of the new free block.
+    // size_t new_block_size = block_size - size;
+
+    // // Ensure the new free block size is aligned.
+    // new_block_size = ALIGN(new_block_size);
+
+    // // Ensure the new free block size is at least as large as the minimum block size.
+    // if (new_block_size < sizeof(memory_block_t)) {
+    //     new_block_size = sizeof(memory_block_t);
+    // }
+
+    // // Calculate the size of the allocated block.
+    // size_t allocated_size = block_size - new_block_size;
+
+    // // Create a new free block from the remaining space.
+    // memory_block_t *new_block = (memory_block_t *)((char *)block + allocated_size);
+    // put_block(new_block, new_block_size, false);  // Mark it as free.
+
+    // // Update the size of the original block.
+    // put_block(block, allocated_size, true);  // Mark it as allocated.
+
+    // // Update the linked list pointers.
+    // new_block->next = block->next;
+    // block->next = new_block;
+
+    // return new_block;  // Return the new free block.
+     assert(block != NULL);
+    size_t block_size = get_size(block);
+    
+    // Calculate the remaining free block size.
+    size_t remaining_size = block_size - size - sizeof(memory_block_t);
+
+    // Check if splitting is possible.
+    if (remaining_size < sizeof(memory_block_t)) {
+        return NULL;  // Not enough space for a new free block.
+    }
+
+    // Create the allocated block from the beginning of the original block.
+    put_block(block, size, true);
+
+    // Create a new free block from the remaining space.
+    memory_block_t *new_free_block = (memory_block_t *)((char *)block + size + sizeof(memory_block_t));
+    put_block(new_free_block, remaining_size, false);
+    
+    // Update the linked list pointers.
+    new_free_block->next = block->next;
+    block->next = new_free_block;
+
+    return block;  // Return the allocated block.
+
 }
 
 /*
@@ -118,7 +206,19 @@ memory_block_t *split(memory_block_t *block, size_t size) {
  */
 memory_block_t *coalesce(memory_block_t *block) {
     //* STUDENT TODO
-    return NULL;
+    assert(block != NULL);
+
+    memory_block_t *next_block = (memory_block_t *)((char *)block + get_size(block));
+
+    if (!is_allocated(next_block)) {
+        // Next block is free, coalesce with it.
+        block->next = next_block->next;
+        block->block_size_alloc += next_block->block_size_alloc;
+    }
+
+    // TODO: Implement coalescing with the previous block if applicable.
+
+    return block;
 }
 
 
@@ -129,7 +229,19 @@ memory_block_t *coalesce(memory_block_t *block) {
  */
 int uinit() {
     //* STUDENT TODO
-    return 0;
+    size_t initial_size = 8192;
+
+    void *initial_heap = csbrk(initial_size);
+    if (initial_heap == NULL) {
+        return -1; 
+    }
+
+    memory_block_t *initial_block = (memory_block_t *)initial_heap;
+    put_block(initial_block, initial_size, 0); 
+
+    free_head = initial_block;
+
+    return 0; 
 }
 
 /*
@@ -137,7 +249,27 @@ int uinit() {
  */
 void *umalloc(size_t size) {
     //* STUDENT TODO
-    return NULL;
+    // Check for invalid size
+    if (size == 0) {
+        return NULL;
+    }
+
+    // Find a suitable free block
+    memory_block_t *block = find(size);
+
+    if (block == NULL) {
+        // No suitable free block found, extend the heap
+        block = extend(size);
+        if (block == NULL) {
+            return NULL; // Heap extension failed
+        }
+    }
+
+    // Allocate the block
+    allocate(block);
+
+    // Return a pointer to the allocated memory
+    return get_payload(block);
 }
 
 /*
@@ -146,4 +278,17 @@ void *umalloc(size_t size) {
  */
 void ufree(void *ptr) {
     //* STUDENT TODO
+    // Check for NULL pointer
+    if (ptr == NULL) {
+        return;
+    }
+
+    // Get the corresponding memory block
+    memory_block_t *block = get_block(ptr);
+
+    // Mark the block as unallocated
+    deallocate(block);
+
+    // Optionally, coalesce the block
+    coalesce(block);
 }
